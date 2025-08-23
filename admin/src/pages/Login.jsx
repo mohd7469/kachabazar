79036bbb6fad9daf4cb61f8836060e6c3b8621ac
mohammad <awais3227443@gmail.com>
2025-08-23 15:07:04 +0400

    changes

--- admin/src/pages/Login.jsx
+++ admin/src/pages/Login.jsx
@@ -40,7 +40,7 @@ const Login = () => {
             <main className="flex items-center justify-center p-6 sm:p-12 md:w-1/2">
               <div className="w-full">
                 <h1 className="mb-6 text-2xl font-semibold text-gray-700 dark:text-gray-200">
-                  Admin Login
+                  Login
                 </h1>
                 <form onSubmit={handleSubmit(onSubmit)}>
                   <LabelArea label="Email" />
@@ -89,14 +89,14 @@ const Login = () => {
                   <hr className="my-10" />
                   {/*<button
                     disabled
-                    className="text-sm inline-flex items-center cursor-pointer transition ease-in-out duration-300 font-semibold font-serif text-center justify-center rounded-md focus:outline-none text-white bg-blue-600 shadow-sm my-2 md:px-2 lg:px-3 py-4 md:py-3.5 lg:py-4 hover:text-white hover:bg-blue-600 h-11 md:h-12 w-full mr-2"
+                    className="text-sm inline-flex items-center cursor-pointer transition ease-in-out duration-300 font-semibold font-serif text-center justify-center rounded-md focus:outline-none text-gray-700 bg-gray-100 shadow-sm my-2 md:px-2 lg:px-3 py-4 md:py-3.5 lg:py-4 hover:text-white hover:bg-blue-600 h-11 md:h-12 w-full mr-2"
                   >
                     <ImFacebook className="w-4 h-4 mr-2" />{" "}
                     <span className="ml-2"> {t("LoginWithFacebook")} </span>
                   </button>
                   <button
                     disabled
-                    className="text-sm inline-flex items-center cursor-pointer transition ease-in-out duration-300 font-semibold font-serif text-center justify-center rounded-md focus:outline-none text-white bg-red-500 shadow-sm my-2  md:px-2 lg:px-3 py-4 md:py-3.5 lg:py-4 hover:text-white hover:bg-red-500 h-11 md:h-12 w-full"
+                    className="text-sm inline-flex items-center cursor-pointer transition ease-in-out duration-300 font-semibold font-serif text-center justify-center rounded-md focus:outline-none text-gray-700 bg-gray-100 shadow-sm my-2  md:px-2 lg:px-3 py-4 md:py-3.5 lg:py-4 hover:text-white hover:bg-red-500 h-11 md:h-12 w-full"
                   >
                     <ImGoogle className="w-4 h-4 mr-2" />{" "}
                     <span className="ml-2">{t("LoginWithGoogle")}</span>
