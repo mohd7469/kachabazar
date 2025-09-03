f819c315125a73e0c5c8381ee6e95c9e43ee39fd
mohammad <awais3227443@gmail.com>
2025-09-03 23:09:33 +0400

    delivery section bg fix

--- store-without-stripe/src/components/offer/OfferCard.js
+++ store-without-stripe/src/components/offer/OfferCard.js
@@ -13,7 +13,7 @@ const OfferCard = () => {
     <div className="w-full group">
       <div className="bg-gray-50 h-full border-2 border-emerald-500 transition duration-150 ease-linear transform group-hover:border-emerald-400 rounded shadow">
         <div className="bg-orange-100 text-gray-900 px-6 py-2 rounded-t border-b flex items-center justify-center">
-          <h3 className="text-base font-serif font-medium ">
+          <h3 className="text-base font-serif font-bold">
             {showingTranslateValue(
               storeCustomizationSetting?.home?.discount_title
             )}
