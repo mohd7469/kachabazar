8f1e863470d012125b5236b16829171cbac40e0f
mohammad <awais3227443@gmail.com>
2025-09-07 22:07:05 +0400

    track order in progress

--- store-without-stripe/src/pages/user/tracking/api.js
+++ store-without-stripe/src/pages/user/tracking/api.js
@@ -1,9 +1,10 @@
 import axios from "axios";
-import { TRACKING_CONFIG } from "./config";
+import TRACKING_CONFIG from "./config";
 
 export async function getTrackingHtml(trackNo) {
   if (!trackNo) throw new Error("tracking number is required");
   const url = `${TRACKING_CONFIG.BASE_URL}?trackno=${encodeURIComponent(trackNo)}`;
+  console.log('url: ', url);
   const { data } = await axios.get(url);
   console.log('data: ', data);
   return data;
